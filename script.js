f99a41d51639fb47c19c8195da8929ddba2ef1c7
OswaldoDPF98 <128750827+OswaldoDPF98@users.noreply.github.com>
2024-07-23 14:58:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+function somar() {
+  var n1 = parseInt(document.getElementById("n1").value);
+  var n2 = parseInt(document.getElementById("n2").value);
+  
+  var resp = document.getElementById("respSoma")
+  resp.textContent = n1 + n2;
+}
+
+function subtração() {
+  var n3 = parseInt(document.getElementById("n3").value);
+  var n4 = parseInt(document.getElementById("n4").value);
+  
+  var resp = document.getElementById("respSub")
+  resp.textContent = n3 - n4;
+}
+
+function divisão() {
+  var n5 = parseInt(document.getElementById("n5").value);
+  var n6 = parseInt(document.getElementById("n6").value);
+  
+  var resp = document.getElementById("respDiv")
+  resp.textContent = n5 / n6;
+}
+
+function multiplicação() {
+  var n7 = parseInt(document.getElementById("n7").value);
+  var n8 = parseInt(document.getElementById("n8").value);
+  
+  var resp = document.getElementById("respMul")
+  resp.textContent = n7 * n8;
+}
+
+function media() {
+  var n9 = parseInt(document.getElementById("n9").value);
+  var n10 = parseInt(document.getElementById("n10").value);
+  
+  var resp = document.getElementById("respMed")
+  resp.textContent = (n9 + n10)/2;
+}
+
+function iteração() {
+  var n1 = parseInt(document.getElementById("n1").value);
+  var n2 = parseInt(document.getElementById("n2").value);
+  
+  var resp = document.getElementById("respIte")
+  resp.textContent = n11 + n12;
+}
